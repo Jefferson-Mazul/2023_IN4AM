2088bbf1f62352f90f0e1c6fe362c708e5347081
Jefferson-Mazul <jmazul-2023430@kinal.edu.gt>
2026-07-21 10:55:15 -0600

    Se crearon los paquetes: view, exceptions, repository, resources y styles

---
+++ 2023430-in4am-3b-exam/src/com/jeffersonmazul/exam3b/exceptions/LoginExceptions.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jeffersonmazul.exam3b.exceptions;
+
+/**
+ *
+ * @author jeff2
+ */
+public class LoginExceptions {
+    
+}
--- 2023430-in4am-3b-exam/src/com/jeffersonmazul/exam3b/model/Usuario.java
+++ 2023430-in4am-3b-exam/src/com/jeffersonmazul/exam3b/model/Usuario.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.jm.controller;
+package com.jeffersonmazul.exam3b.model;
 
 /**
  *
  * @author jeff2
  */
-public class SceneManager {
+public class Usuario {
     
 }
