5b7ca4b808810d6879122898476c6204cea2b557
Jefferson-Mazul <jmazul-2023430@kinal.edu.gt>
2026-07-21 09:21:58 -0600

    Se creo el paqeute controller que contiene el scenemanager controller y se agrego el paquete system donde ira el main

--- 2023430-in4am-3b-exam/src/com/jeffersonmazul/exam3b/system/Main.java
+++ 2023430-in4am-3b-exam/src/com/jeffersonmazul/exam3b/system/Main.java
@@ -2,19 +2,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package com.jm.system;
+package com.jeffersonmazul.exam3b.system;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 /**
  *
  * @author jeff2
  */
-public class Main {
+public class Main extends Application {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        launch(args);
     }
-    
+
+    @Override
+    public void start(Stage escenarioRaiz) {
+
+    }
 }
